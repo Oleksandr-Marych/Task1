39ad8e66ce4c95de4bd6c2da90122c36c9825e39
Oleksandr-Marych <162808223+Oleksandr-Marych@users.noreply.github.com>
2024-03-18 12:25:08 +0100

    new feature added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Task1;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification1");
 Console.WriteLine("Modification2");
 Console.WriteLine("Modification3");
+
+int[] array = { 2, 4, 6, 8, 10 };
+double average = Task2.Average(array); 
+Console.WriteLine("The average is: " + average);
+
---
+++ Task1/Task2.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1;
+
+public class Task2
+{
+    public static double Average(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int num in numbers) {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+}
