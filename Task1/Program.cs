cc7a18c65836632d8e50fa6d02ae853bbabe9057
Oleksandr-Marych <162808223+Oleksandr-Marych@users.noreply.github.com>
2024-03-18 12:55:40 +0100

    rebasing

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,5 +9,5 @@ Console.WriteLine("Modification3");
 
 int[] array = { 2, 4, 6, 8, 10 };
 Console.WriteLine("The average is: " + Task2.Average(array));
-
 Console.WriteLine("The maximal value is: " + Task3.Max(array));
+Console.WriteLine("Rebasing");
